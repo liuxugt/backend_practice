1eda675b4ab5d01e56b5f8b5b59d2d37cae2e3f0
Xu Liu <liuxugt@gmail.com>
2019-04-22 09:56:22 -0400

    define const classes

---
+++ src/main/java/com/example/struct/session.java
@@ -0,0 +1,40 @@
+package com.example.struct;
+
+public class session {
+    String start_time;
+    String end_time;
+    int counter;
+
+    public session(){};
+
+    public session(String start_time, String end_time, int counter) {
+        this.start_time = start_time;
+        this.end_time = end_time;
+        this.counter = counter;
+    }
+
+    public void setStart_time(String start_time) {
+        this.start_time = start_time;
+    }
+
+    public void setEnd_time(String end_time) {
+        this.end_time = end_time;
+    }
+
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
+    public String getStart_time() {
+        return start_time;
+    }
+
+    public String getEnd_time() {
+        return end_time;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+}
---
+++ src/main/java/com/example/struct/user.java
@@ -0,0 +1,50 @@
+package com.example.struct;
+
+public class user {
+    String first_name;
+    String last_name;
+    String email;
+    int id = 0;
+
+    user(){
+        first_name = "";
+        last_name = "";
+        id = 0;
+        email = "";
+    }
+    user(String first_name, String last_name, String email){
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+    }
+    public String getFirst_name(){
+        return first_name;
+    }
+    public String getLast_name(){
+        return last_name;
+    }
+    public String getEmail(){
+        return email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setFirst_name(String first_name){
+        this.first_name = first_name;
+    }
+    public void setLast_name(String last_name){
+        this.last_name = last_name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+
+}
