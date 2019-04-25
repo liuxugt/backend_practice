3ccbafed1f4c06466ac44a84dd143a01ae28b3c1
Xu Liu <liuxugt@gmail.com>
2019-04-25 09:22:35 -0400

    finish mapper.xml and mapper.java

--- src/main/java/com/xu/database/Mapper.java
+++ src/main/java/com/xu/database/Mapper.java
@@ -1,6 +1,29 @@
 package com.xu.database;
 
+import com.xu.struct.Project;
+import com.xu.struct.Session;
+import com.xu.struct.User;
+import org.apache.ibatis.annotations.Param
+
+import java.util.List;
+
 public interface Mapper {
 
+    int createUser(@Param("firstname") String firstName, @Param("lastname") String lastname, @Param("email") String email);
+    void updateUser(@Param("id") int uid, @Param("firstname") String firstname, @Param("lastname") String lastname);
+    void deleteUser(@Param("id") int uid);
+    User getUser(@Param("id") int uid);
+    List<User> getAllUsers();
+    List<String> getAllEmails();
 
+    int createProject(@Param("uid") int uid, @Param("name") String projectName);
+    void updateProject(@Param("uid") int uid, @Param("pid") int pid, @Param("name") String projectName);
+    void deleteProject(@Param("uid") int uid, @Param("pid") int pid);
+    Project getProject(@Param("uid") int uid, @Param("pid") int pid);
+    List<Project> getAllProjects(@Param("uid") int uid );
+
+    int createSession(@Param("uid") int uid, @Param("pid") int pid, @Param("start_time")String startTime, @Param("end_time") String endTime, @Param("counter") int counter);
+    void updateSession(@Param("uid") int uid, @Param("pid") int pid, @Param("sid") int sid, @Param("start_time")String startTime, @Param("end_time") String endTime, @Param("counter") int counter);
+    Session getSession(@Param("uid") int uid, @Param("pid") int pid, @Param("sid") int sid);
+    List<Session> getAllSessions(@Param("uid") int uid, @Param("pid") int pid);
 }
